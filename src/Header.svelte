9e75a6478428c084c3ead43a3fa979c4d89db5b6
Fernando <nandosbx@users.noreply.github.com>
2021-09-30 20:22:14 -0300

    User Auth

---
+++ src/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { username, user } from './user'
+
+    function signout() {
+        user.leave()
+        username.set('')
+    }
+</script>
+
+<header>
+    <h1>Gun</h1>
+    {#if $username}
+        <div class="user-bio">
+            <span>Hello <strong>{$username}</strong></span>
+
+            <img
+                src={`https://avatar.decibear.com/api/initials/${$username}.svg`}
+                alt=""
+            />
+        </div>
+
+        <button class="signout-button" on:click={signout}>Sign Out</button>
+    {:else}
+        <h3>Gun.js Chat</h3>
+    {/if}
+</header>
